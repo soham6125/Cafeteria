04946fbdd17b425f489fd531a5af459fae012d5e
soham6125 <chaudhuri.ber@gmail.com>
2021-12-31 18:29:05 +0530

    update

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from 'react'
+import { React, useState } from 'react'
 import { motion } from 'framer-motion';
 import MenuItem from '../components/MenuItem'
 import { MenuList } from '../helpers/MenuList'
