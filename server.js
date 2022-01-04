9451f5278c066ab5d6f36c09e6f58f2efd8bd05f
soham6125 <chaudhuri.ber@gmail.com>
2022-01-05 00:44:42 +0530

    update

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const { fstat, readFileSync } = require('fs');
+const path = require( 'path' );
+const cors = require( 'cors' );
 
 const app = express();
+app.use(cors());
+const data = JSON.parse(readFileSync(path.resolve('./db.json'), 'utf-8'));
 const PORT = process.env.PORT || 3001;                 
 app.use(express.static('public')); 
 app.use('/images', express.static('images'));
+app.get('/data', (req, res) => {
+  return res.json(data);
+})
 app.listen(PORT, () => {
   console.log(`Running server on PORT ${PORT}...`);
 })
