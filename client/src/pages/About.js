f2e784e0d23b5f6daeb194c156c098b1c852e60d
soham6125 <chaudhuri.ber@gmail.com>
2022-01-05 00:48:34 +0530

    update

---
+++ client/src/pages/About.js
@@ -0,0 +1,39 @@
+import { motion } from 'framer-motion'
+import {React, useState} from 'react'
+import Image from '../assets/about_us.jpg'
+import Popup from '../components/Popup'
+import '../styles/About.css'
+
+function About() {
+    const [buttonPopup, setButtonPopup] = useState(false);
+
+    return (
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+            <div className='about'>
+                <div className='aboutTop' style={{backgroundImage: `url(${Image})`}}></div>
+                <div className='aboutBottom'>
+                    <h1> Our Story </h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+                        mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+                        voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+                        fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
+                        accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
+                        molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
+                        officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
+                        nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
+                        error repudiandae fuga? Ipsa laudantium molestias eos sapiente
+                        officiis modi at sunt excepturi expedita sint? Sed quibusdam
+                        recusandae alias error harum maxime adipisci amet laborum.
+                        Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a
+                        cumque velit
+                    </p>
+                    <button className='btn-about from-left' onClick={() => setButtonPopup(true)}> View Full Story </button>
+                </div>
+                { buttonPopup && <Popup setButtonPopup={setButtonPopup} /> }
+            </div>
+        </motion.div>
+    )
+}
+
+export default About
---
+++ client/src/pages/Menu.js
@@ -0,0 +1,81 @@
+import { React, useState, useEffect } from 'react'
+import { motion } from 'framer-motion';
+import MenuItem from '../components/MenuItem'
+import '../styles/Menu.css'
+import { Select, MenuItem as Option, InputLabel, FormControl } from '@mui/material';
+
+function Menu() {
+    const [data, setData] = useState(null)
+    const [query, setQuery] = useState("")
+    const [sortType, setSortType] = useState('');
+
+    useEffect(() => {
+        fetch("http://localhost:3001/data")
+        .then(res => {
+          if(!res.ok) {
+            throw Error("Could not fetch data");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setData(data.MenuList);
+        })
+    }, [])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    const handleSubmitTwo = (e) => {
+        setSortType(() => {
+            if(e.target.value === 'price') setData((data) => data.sort((a, b) => a.price > b.price ? 1 : -1));
+            else if(e.target.value === 'name') setData((data) => data.sort((a, b) => a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1));
+            return e.target.value;
+        });
+    }
+
+    return (
+        <div>
+            {data && 
+            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+            <div className='menu'>
+                <h1 className='menuTitle'> Our Menu </h1>
+                <div className='search-bar' style={{ display: "flex", alignItems: "center", width: "60vw", justifyContent: "space-between" }}>
+                    <form onSubmit={handleSubmit} id="search-box" style={{ display: "flex", flexGrow: 2, flexDirection: "row", alignItems: "center" }}>
+                        <input 
+                         type="text" 
+                         className="input-search" 
+                         placeholder="Search for your favourite coffee..."
+                         value={query}
+                         onChange={(e) => setQuery(e.target.value)}
+                        />
+                        <button className="btn-search"><i className="fa fa-search"></i></button>
+                    </form>
+                    <div>
+                        <FormControl fullWidth>
+                            <InputLabel id="select-label"> Sort by: </InputLabel>
+                            <Select
+                             style={{ minWidth: 150, marginBottom: 20, height: 50, backgroundColor: 'white' }}
+                             labelId="select-label"
+                             label="Sort by:"
+                             value={sortType} 
+                             onChange={handleSubmitTwo}
+                            >
+                                <Option value={'name'}> Name </Option>
+                                <Option value={'price'}> Price </Option>
+                            </Select>
+                        </FormControl>
+                    </div>
+                </div>
+                <div className='menuList'>
+                    {data.filter(coffee => (coffee.name.toLowerCase().includes(query.toLowerCase()))).map((menuItem, key) => {
+                        return <MenuItem key={key} name={menuItem.name} image={"http://localhost:3001/images/" + menuItem.image.substring(10)} price={menuItem.price} />
+                    })}
+                </div>
+            </div>
+            </motion.div>}
+        </div>
+    )
+}
+
+export default Menu
