79ded33fafc757e44d14999951a31220beb5143c
soham6125 <chaudhuri.ber@gmail.com>
2022-01-05 12:57:08 +0530

    update

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,57 @@
+import Espresso from '../assets/Espresso.gif';
+import Cappuccino from '../assets/Cappuccino.jpg';
+import Glace from '../assets/Glace.jpg';
+import Latte from '../assets/Latte.jpg';
+import Cocoa from '../assets/Cocoa.jpg';
+import Marochinno from '../assets/Marochinno.jpg';
+import Americano from '../assets/Americano.jpg';
+import Antoccino from '../assets/Antoccino.jpg';
+import Mocha from '../assets/Mocha.jpg';
+
+export const MenuList = [
+    {
+      "name": "Espresso",
+      "image": Espresso,
+      "price": 200
+    },
+    {
+      "name": "Cappuccino",
+      "image": Cappuccino,
+      "price": 215
+    },
+    {
+      "name": "Glace",
+      "image": Glace,
+      "price": 150
+    },
+    {
+      "name": "Latte",
+      "image": Latte,
+      "price": 175
+    },
+    {
+      "name": "Cocoa",
+      "image": Cocoa,
+      "price": 300
+    },
+    {
+      "name": "Marochinno",
+      "image": Marochinno,
+      "price": 225
+    },
+    {
+      "name": "Americano",
+      "image": Americano,
+      "price": 175
+    },
+    {
+      "name": "Antoccino",
+      "image": Antoccino,
+      "price": 205
+    },
+    {
+      "name": "Mocha",
+      "image": Mocha,
+      "price": 195
+    }
+];
