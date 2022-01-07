999c0c1cf8dab22e2fd52448a45bb2ae19436746
soham6125 <chaudhuri.ber@gmail.com>
2022-01-07 09:40:45 +0530

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,37 @@
-import React from 'react'
-import '../styles/Home.css'
-import { motion } from 'framer-motion';
+import React from "react";
+import "../styles/Home.css";
+import { motion } from "framer-motion";
+import { Link } from "react-scroll";
 
 function Home() {
-    return (
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
-            <div className='home'>
-                <div className='headerContainer'>
-                    <h1> Cafeteria </h1>
-                    <p> Let us make your day! </p>
-                    <a href="#menu" style={{textDecoration: 'none'}}>
-                        <button className="primary-button" id="order_btn"><span>Order Now</span></button>
-                    </a>
-                </div>
-            </div>
-        </motion.div>
-    )
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      <div className="home">
+        <div className="headerContainer">
+          <h1> Cafeteria </h1>
+          <p> Let us make your day! </p>
+          <a href="#menu" style={{ textDecoration: "none" }}>
+            <Link
+              to="menu"
+              activeClass="active"
+              smooth={true}
+              duration={500}
+              style={{ fontSize: 15 }}
+            >
+              {" "}
+              <button className="primary-button" id="order_btn">
+                <span>Order Now</span>
+              </button>{" "}
+            </Link>
+          </a>
+        </div>
+      </div>
+    </motion.div>
+  );
 }
 
-export default Home
+export default Home;
